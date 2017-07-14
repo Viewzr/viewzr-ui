943aeb4bcd64dafa2420644c3f404241c25caff8
scottviewzr <scott@viewzr.com>
2017-07-14 12:05:12 -0600

    header-bar ready for initial code review

---
+++ header-bar/src/pages/creator-dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class CreatorDashboard extends Component {
+    render() {
+        return (
+            <div>
+                CreatorDashboard Page
+            </div>
+        );
+    }
+}
+export default CreatorDashboard;
